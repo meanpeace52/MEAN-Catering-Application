7f835a89af0cdf455db1b715bfa04a1c0796b143
solar-dev23 <solar.dev023@gmail.com>
2016-09-20 23:03:10 +0200

    Update - SSL again.

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ mongoose.connection.on('error', function(err) {
 if (config.seedDB) { require('./config/seed'); }
 
 // Setup server
-var privateKey  = fs.readFileSync('dev.cateringninja.com.key.key');
+var privateKey  = fs.readFileSync('dev.cateringninja.com.key');
 var certificate = fs.readFileSync('app_cateringninja_com.crt');
 // var privateKey  = fs.readFileSync('dev.cateringninja.com.key');
 // var certificate = fs.readFileSync('dev_cateringninja_com.crt');
