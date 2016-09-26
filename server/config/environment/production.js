256112b5b9414bed32a5c40e2ad42972835c0271
alternativex <sean@workaroundme.com>
2016-09-25 23:35:57 -0400

    new dwolla codes

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -36,11 +36,11 @@ module.exports = {
       API_KEY: 'E1A33450-A519-4379-8FF8-CBA269A17640'
     },
     DWOLLA: {
-      KEY: '2jKQn8aERXipDkCuNYG8TKMKPqpGB4KjuK1Gevrw74ZITIK6Ew',
-      SECRET: 'tWsvZmICKSRpbGbO0WWRKBVXb4B03j35XpI5ifrP1LJJJ5HExy',
+      KEY: 'EVOeQHO2RWEoi57XF80Fxhj1CyfIGIngMCx7vEBCzbhOEl9D0a',
+      SECRET: '78CmxYBXv0DBBv49J18eeCBKxdh75l0OfgZk20pTr7nOmmvGqa',
       ACCOUNT_ID: 'f8615f27-0783-4078-97db-d8d7ce3ae368', //eabd60f1-8fbc-4ed9-b09a-b213bdc9d092
-      ACCESS_TOKEN: 'ZT23sJndZj9OHRME4pm6qAZXkNWonkBfgdZvmAVS4glfkhJJ91',
-      REFRESH_TOKEN: 'QNE9juhsyxEOYV9LyMM5OuxAWQIHmcxm6jlt8Eo5fiMxjXJiOw',
+      ACCESS_TOKEN: 'IrdNPy4iG37MOOAGuqmCn44FeWMPhunk96QzS2eds5J4gs836D',
+      REFRESH_TOKEN: 'acc7964c-6bb9-45b2-bbad-5b91487c6758',
       LOGIN_URL: 'https://dwolla.com/login'
     },
     STRIPE: {
