d8f5debfc0197bfd66fa846a6613b21a0a541bd9
alternativex <sean@workaroundme.com>
2016-10-21 23:27:51 -0400

    remove other changes

--- server/routes.js
+++ server/routes.js
@@ -34,16 +34,7 @@ export default function(app) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
-  app.get('/', function(req, res, next) {
-    // Handle the get for this route
-  });
-
-  app.post('/', function(req, res, next) {
-    // Handle the post for this route
-  });
-
-
+  
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app)/*')
     .get(errors[404]);
